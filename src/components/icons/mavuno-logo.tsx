bf06335b70e2ec3168e4b68043b582e6245d24bd
Collins Lagat <collinslagat1901@gmail.com>
2025-09-27 07:11:18 +0000

    find a better icon to use

--- src/components/icons/mavuno-logo.tsx
+++ src/components/icons/mavuno-logo.tsx
@@ -20,8 +20,9 @@ const MavunoLogo = ({ showText = true, ...props }: MavunoLogoProps) => (
       className={cn('text-primary', props.className)}
       {...props}
     >
-        <path d="M2 22a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2zM20 16.3A5.4 5.4 0 0 0 17.5 15a5.4 5.4 0 0 0-2.5 1.3A5.4 5.4 0 0 0 12.5 15a5.4 5.4 0 0 0-2.5 1.3A5.4 5.4 0 0 0 7.5 15a5.4 5.4 0 0 0-2.5 1.3V10c0-4.4 3-8 7-8s7 3.6 7 8v6.3z" />
-        <path d="M11.5 2c0 2.8.8 5.2 2.5 7" />
+        <path d="M2 22s10-3 10-10S2 2 2 2"/>
+        <path d="M22 22s-10-3-10-10S22 2 22 2"/>
+        <path d="M12 2v20"/>
     </svg>
     {showText && <span className="text-xl font-bold text-current">Mavuno</span>}
   </div>
